f3c0f32a85add1713ed4cb8e073ee230870bf291
langresser <wangjiatc@gmail.com>
2012-02-24 00:13:10 +0800

    Initial Commit

---
+++ ShenxiandaoHelper/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ShenxiandaoHelper
+//
+//  Created by 王 佳 on 12-2-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
